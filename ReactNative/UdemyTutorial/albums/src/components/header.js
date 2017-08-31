43bed5ca7d0787de1621e96e77546c9e6bb488da
Akila <makila@amazon.com>
2017-08-31 15:36:43 +0530

    Seperate header component for Albums app. Styling the Component in react native.

---
+++ ReactNative/UdemyTutorial/albums/src/components/header.js
@@ -0,0 +1,17 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a Component
+const Header = () => {
+  const { textStyle } = styles;
+  return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
+// Make the component available to other parts of the app.
+export default Header;
