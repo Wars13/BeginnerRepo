d9de65933999f15379a8d04269ebd0a550f79838
Akila <makila@amazon.com>
2017-09-03 15:08:34 +0530

    Styling a card.

--- ReactNative/UdemyTutorial/albums/src/components/Card.js
+++ ReactNative/UdemyTutorial/albums/src/components/Card.js
@@ -3,9 +3,31 @@ import { View } from 'react-native';
 
 const Card = () => {
   return (
-    <View>
+    <View style={styles.containerStyle}>
     </View>
   );
+};
+
+const styles = {
+  //'containerStyle' (or) can be any name - names for styles property are completely arbitrary
+  containerStyle : {
+    //border
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+
+    //shadow
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.9,
+    shadowRadius: 2,
+
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
 };
 
 export default Card;
