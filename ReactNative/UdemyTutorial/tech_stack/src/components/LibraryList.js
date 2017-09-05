bbc40491d24561e2ba0c41d2da0ae813d04b546b
Akila <makila@amazon.com>
2017-09-05 14:52:34 +0530

    MapStateToProps. Initial code.. See console to ensure the function getting called with state..

--- ReactNative/UdemyTutorial/tech_stack/src/components/LibraryList.js
+++ ReactNative/UdemyTutorial/tech_stack/src/components/LibraryList.js
@@ -7,7 +7,13 @@ class LibraryList extends Component {
   }
 }
 
-export default connect()(LibraryList);
+// function takes global state object (that sits in our Redux store)
+// and proivde them as Props to our LibraryList.
+const mapStateToProps = state => {
+  console.log(state);
+};
+
+export default connect(mapStateToProps)(LibraryList);
 //it calls the function connect .. connect returns another function..
 //we then immediately call that function with LibraryList.
 
