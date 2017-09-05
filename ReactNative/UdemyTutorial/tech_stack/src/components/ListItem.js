170a381e5f4edc35c38122669e7a8539ae685bb8
Akila <makila@amazon.com>
2017-09-05 16:44:28 +0530

    Calling action creators. Ensure call using console log.. run here

--- ReactNative/UdemyTutorial/tech_stack/src/components/ListItem.js
+++ ReactNative/UdemyTutorial/tech_stack/src/components/ListItem.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions'; // * - import all action creators as actions..
+
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
-
+    console.log(this.props);
+    
     return (
       <CardSection>
         <Text style={titleStyle}>
@@ -23,4 +27,6 @@ const styles = {
   }
 };
 
-export default ListItem;
+//first argument of connect is exclusively for mapStateToProps..
+//second arg..pass actions to the ListItem component as Props.
+export default connect(null, actions)(ListItem);
