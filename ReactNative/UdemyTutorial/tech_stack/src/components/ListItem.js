1256d53474a3a185daae3edd13a95d1a1afd6a45
Akila <makila@amazon.com>
2017-09-05 17:57:25 +0530

    Expanding a row. OnPress check the selected id and render the description.

--- ReactNative/UdemyTutorial/tech_stack/src/components/ListItem.js
+++ ReactNative/UdemyTutorial/tech_stack/src/components/ListItem.js
@@ -5,10 +5,20 @@ import { CardSection } from './common';
 import * as actions from '../actions'; // * - import all action creators as actions..
 
 class ListItem extends Component {
+
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+
+    if (library.id === selectedLibraryId) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library;
-    
     return (
       <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
         <View>
@@ -17,6 +27,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -30,6 +41,12 @@ const styles = {
   }
 };
 
+const mapStateToProps = state => {
+  // anyName : state.selectedLibraryId because
+  //SelectedReducer is wired up to the key selectedLibraryId in the index.js of reducers.
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
 //first argument of connect is exclusively for mapStateToProps..
 //second arg..pass actions to the ListItem component as Props.
-export default connect(null, actions)(ListItem);
+export default connect(mapStateToProps, actions)(ListItem);
