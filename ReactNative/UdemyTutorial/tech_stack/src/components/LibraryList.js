3a4e448dee74bec6d9c7b9e27e1aa3c8e82495f4
Akila <makila@amazon.com>
2017-09-05 15:41:33 +0530

    Rendering a single row - Seperate Component..

--- ReactNative/UdemyTutorial/tech_stack/src/components/LibraryList.js
+++ ReactNative/UdemyTutorial/tech_stack/src/components/LibraryList.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
 
-  // componentWillMount - lifecycle method..
-  // all we have to do is define it.
   componentWillMount() {
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2
     });
-    //when we create ListView, we have to tel what DataSource it has to use
+
     this.dataSource = ds.cloneWithRows(this.props.libraries);
   }
-
-  // renderRow - arbitray method name
-  //used to insturct ListView how to render the list with property renderRow=<method>
-  renderRow() {
-    
+//library - element that it is trying to render currently
+  renderRow(library) {
+    //library - props name.. can also be anyName={library}
+    return <ListItem library={library} />;
   }
 
   render() {
