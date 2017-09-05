25490096858e3c26f04571f1304bfa3cfdfaf8c3
Akila <makila@amazon.com>
2017-09-05 17:47:32 +0530

    Rules of Reducers. Add switch statement for diff action types.

--- ReactNative/UdemyTutorial/tech_stack/src/reducers/SelectionReducer.js
+++ ReactNative/UdemyTutorial/tech_stack/src/reducers/SelectionReducer.js
@@ -1,4 +1,12 @@
-export default (state, action) => {
-  console.log(action);
-  return null;
+//very first time reducer runs, return some non undefined value from the reducer.
+//first time it runs, state will be called with value 'undefined', so set it to null.
+//so this sets to null if it is undefined.
+//i.e when user doen't touch on any library at the app start time, make it null.
+export default (state = null, action) => {
+  switch (action.type) {
+    case 'select_library':
+      return action.payload;
+    default:
+      return state; //return whatever the state was already in.
+  }
 };
